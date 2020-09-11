eaf251b96164864995f3aaf84ac74a9761821ca9
Raphaël Chalicarne <raphael.chalicarne@ecl17.ec-lyon.fr>
2020-09-11 21:29:08 +0200

    Format file

--- lang/lang.js
+++ lang/lang.js
@@ -3,20 +3,18 @@ var langCode = '';
 var langJS = null;
 
 
-var translate = function (jsdata)
-{	
-	$("[tkey]").each (function (index)
-	{
-		var strTr = jsdata [$(this).attr ('tkey')];
-	    $(this).html (strTr);
+var translate = function (jsdata) {
+	$("[tkey]").each(function (index) {
+		var strTr = jsdata[$(this).attr('tkey')];
+		$(this).html(strTr);
 	});
 }
 
 
-langCode = navigator.language.substr (0, 2);
+langCode = navigator.language.substr(0, 2);
 
 if (langs.includes(langCode)) {
-	$.getJSON('lang/'+langCode+'.json', translate);
+	$.getJSON('lang/' + langCode + '.json', translate);
 } else {
 	$.getJSON('lang/en.json', translate);
 }
