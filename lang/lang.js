6c44677f2aa6184f75e87c5993d6e2524abab956
raphaelchalicarne <39592819+raphaelchalicarne@users.noreply.github.com>
2025-09-19 09:41:42 +0200

    :wastebasket: Update deprecated method

--- lang/lang.js
+++ lang/lang.js
@@ -11,7 +11,7 @@ var translate = function (jsdata) {
 }
 
 
-langCode = navigator.language.substr(0, 2);
+langCode = navigator.language.substring(0, 2);
 
 if (langs.includes(langCode)) {
 	$.getJSON('lang/' + langCode + '.json', translate);
