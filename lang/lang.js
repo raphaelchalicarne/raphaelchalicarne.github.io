e8db29d7826b52ef35ffadefd70f041fcdf079ca
raphaelchalicarne <39592819+raphaelchalicarne@users.noreply.github.com>
2025-09-19 10:10:34 +0200

    :heavy_minus_sign: Remove jQuery

--- lang/lang.js
+++ lang/lang.js
@@ -3,16 +3,24 @@ var langCode = '';
 var langJS = null;
 
 var translate = function (jsdata) {
-	$("[tkey]").each(function (index) {
-		var strTr = jsdata[$(this).attr('tkey')];
-		$(this).html(strTr);
+	document.querySelectorAll('[tkey]').forEach(function (node) {
+		var strTr = jsdata[node.getAttribute('tkey')];
+		node.innerHTML = strTr;
 	});
 }
 
 langCode = navigator.language.substring(0, 2);
 
+var lang_request = new XMLHttpRequest();
+lang_request.onreadystatechange = function () {
+	if (this.readyState == 4 && this.status == 200) {
+		translate(lang_request.response);
+	}
+};
 if (langs.includes(langCode)) {
-	$.getJSON('lang/' + langCode + '.json', translate);
+	lang_request.open('get', 'lang/' + langCode + '.json', true);
 } else {
-	$.getJSON('lang/en.json', translate);
+	lang_request.open('get', 'lang/en.json', true);
 }
+lang_request.responseType = 'json';
+lang_request.send();
