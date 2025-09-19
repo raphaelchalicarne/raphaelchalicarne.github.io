e9c0ac520328ecb7fd860d0674ad9edf08feb704
raphaelchalicarne <39592819+raphaelchalicarne@users.noreply.github.com>
2025-09-19 09:44:51 +0200

    :art: Remove empty lines

--- lang/lang.js
+++ lang/lang.js
@@ -2,14 +2,12 @@ var langs = ['en', 'fr'];
 var langCode = '';
 var langJS = null;
 
-
 var translate = function (jsdata) {
 	$("[tkey]").each(function (index) {
 		var strTr = jsdata[$(this).attr('tkey')];
 		$(this).html(strTr);
 	});
 }
-
 
 langCode = navigator.language.substring(0, 2);
 
@@ -18,6 +16,3 @@ if (langs.includes(langCode)) {
 } else {
 	$.getJSON('lang/en.json', translate);
 }
-
-
-
