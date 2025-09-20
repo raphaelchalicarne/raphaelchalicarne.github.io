528af2974012455655bd427964911a9d5466bd9b
raphaelchalicarne <39592819+raphaelchalicarne@users.noreply.github.com>
2025-09-20 10:45:12 +0200

    :art: Format typescript

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -6,27 +6,27 @@ import {
   FlagIcon,
   MapIcon,
   SparklesIcon,
-} from '@heroicons/react/24/outline';
+} from "@heroicons/react/24/outline";
 
-import GithubIcon from '../components/Icon/GithubIcon';
-import InstagramIcon from '../components/Icon/InstagramIcon';
-import LinkedInIcon from '../components/Icon/LinkedInIcon';
-import StackOverflowIcon from '../components/Icon/StackOverflowIcon';
-import TwitterIcon from '../components/Icon/TwitterIcon';
-import heroImage from '../images/header-background.webp';
-import porfolioImage1 from '../images/portfolio/portfolio-1.jpg';
-import porfolioImage2 from '../images/portfolio/portfolio-2.jpg';
-import porfolioImage3 from '../images/portfolio/portfolio-3.jpg';
-import porfolioImage4 from '../images/portfolio/portfolio-4.jpg';
-import porfolioImage5 from '../images/portfolio/portfolio-5.jpg';
-import porfolioImage6 from '../images/portfolio/portfolio-6.jpg';
-import porfolioImage7 from '../images/portfolio/portfolio-7.jpg';
-import porfolioImage8 from '../images/portfolio/portfolio-8.jpg';
-import porfolioImage9 from '../images/portfolio/portfolio-9.jpg';
-import porfolioImage10 from '../images/portfolio/portfolio-10.jpg';
-import porfolioImage11 from '../images/portfolio/portfolio-11.jpg';
-import photo_profil_Malt from '../images/photo_profil_Malt.png';
-import testimonialImage from '../images/testimonial.webp';
+import GithubIcon from "../components/Icon/GithubIcon";
+import InstagramIcon from "../components/Icon/InstagramIcon";
+import LinkedInIcon from "../components/Icon/LinkedInIcon";
+import StackOverflowIcon from "../components/Icon/StackOverflowIcon";
+import TwitterIcon from "../components/Icon/TwitterIcon";
+import heroImage from "../images/header-background.webp";
+import porfolioImage1 from "../images/portfolio/portfolio-1.jpg";
+import porfolioImage2 from "../images/portfolio/portfolio-2.jpg";
+import porfolioImage3 from "../images/portfolio/portfolio-3.jpg";
+import porfolioImage4 from "../images/portfolio/portfolio-4.jpg";
+import porfolioImage5 from "../images/portfolio/portfolio-5.jpg";
+import porfolioImage6 from "../images/portfolio/portfolio-6.jpg";
+import porfolioImage7 from "../images/portfolio/portfolio-7.jpg";
+import porfolioImage8 from "../images/portfolio/portfolio-8.jpg";
+import porfolioImage9 from "../images/portfolio/portfolio-9.jpg";
+import porfolioImage10 from "../images/portfolio/portfolio-10.jpg";
+import porfolioImage11 from "../images/portfolio/portfolio-11.jpg";
+import photo_profil_Malt from "../images/photo_profil_Malt.png";
+import testimonialImage from "../images/testimonial.webp";
 import {
   About,
   ContactSection,
@@ -38,13 +38,13 @@ import {
   Social,
   TestimonialSection,
   TimelineItem,
-} from './dataDef';
+} from "./dataDef";
 
 /**
  * Page meta data
  */
 export const homePageMeta: HomepageMeta = {
-  title: 'React Resume Template',
+  title: "React Resume Template",
   description: "Example site built with Tim Baker's react resume template",
 };
 
@@ -52,14 +52,14 @@ export const homePageMeta: HomepageMeta = {
  * Section definition
  */
 export const SectionId = {
-  Hero: 'hero',
-  About: 'about',
-  Contact: 'contact',
-  Portfolio: 'portfolio',
-  Resume: 'resume',
-  Skills: 'skills',
-  Stats: 'stats',
-  Testimonials: 'testimonials',
+  Hero: "hero",
+  About: "about",
+  Contact: "contact",
+  Portfolio: "portfolio",
+  Resume: "resume",
+  Skills: "skills",
+  Stats: "stats",
+  Testimonials: "testimonials",
 } as const;
 
 export type SectionId = (typeof SectionId)[keyof typeof SectionId];
@@ -69,31 +69,31 @@ export type SectionId = (typeof SectionId)[keyof typeof SectionId];
  */
 export const heroData: Hero = {
   imageSrc: heroImage,
-  name: `I'm Tim Baker.`,
+  name: `I'm Raphaël Chalicarne.`,
   description: (
     <>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        I'm a Victoria based <strong className="text-stone-100">Full Stack Software Engineer</strong>, currently working
-        at <strong className="text-stone-100">Instant Domains</strong> helping build a modern, mobile-first, domain
-        registrar and site builder.
+        I'm a Lyon based{" "}
+        <strong className="text-stone-100">Back-end Software Engineer</strong>,
+        currently looking for a full-time position.
       </p>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        In my free time time, you can catch me training in <strong className="text-stone-100">Muay Thai</strong>,
-        plucking my <strong className="text-stone-100">banjo</strong>, or exploring beautiful{' '}
-        <strong className="text-stone-100">Vancouver Island</strong>.
+        In my free time time, you can catch me{" "}
+        <strong className="text-stone-100">cycling</strong>, or playing{" "}
+        <strong className="text-stone-100">chess</strong>.
       </p>
     </>
   ),
   actions: [
     {
-      href: '/assets/resume.pdf',
-      text: 'Resume',
+      href: "/assets/resume.pdf",
+      text: "Resume",
       primary: true,
       Icon: ArrowDownTrayIcon,
     },
     {
       href: `#${SectionId.Contact}`,
-      text: 'Contact',
+      text: "Contact",
       primary: false,
     },
   ],
@@ -108,12 +108,20 @@ export const aboutData: About = {
   to use or feel most comfortable with, describing your personality, or whatever else you feel like throwing
   in.`,
   aboutItems: [
-    {label: 'Location', text: 'Victoria, BC', Icon: MapIcon},
-    {label: 'Age', text: '29', Icon: CalendarIcon},
-    {label: 'Nationality', text: 'Canadian / Irish', Icon: FlagIcon},
-    {label: 'Interests', text: 'Motorcycles, Muay Thai, Banjos', Icon: SparklesIcon},
-    {label: 'Study', text: 'University of Victoria', Icon: AcademicCapIcon},
-    {label: 'Employment', text: 'Instant Domains, inc.', Icon: BuildingOffice2Icon},
+    { label: "Location", text: "Victoria, BC", Icon: MapIcon },
+    { label: "Age", text: "29", Icon: CalendarIcon },
+    { label: "Nationality", text: "Canadian / Irish", Icon: FlagIcon },
+    {
+      label: "Interests",
+      text: "Motorcycles, Muay Thai, Banjos",
+      Icon: SparklesIcon,
+    },
+    { label: "Study", text: "University of Victoria", Icon: AcademicCapIcon },
+    {
+      label: "Employment",
+      text: "Instant Domains, inc.",
+      Icon: BuildingOffice2Icon,
+    },
   ],
 };
 
@@ -122,69 +130,73 @@ export const aboutData: About = {
  */
 export const skills: SkillGroup[] = [
   {
-    name: 'Spoken languages',
+    name: "Spoken languages",
     skills: [
       {
-        name: 'English',
+        name: "French",
         level: 10,
       },
       {
-        name: 'French',
+        name: "English",
+        level: 7,
+      },
+      {
+        name: "German",
         level: 4,
       },
       {
-        name: 'Spanish',
-        level: 3,
+        name: "Portuguese",
+        level: 4,
       },
     ],
   },
   {
-    name: 'Frontend development',
+    name: "Frontend development",
     skills: [
       {
-        name: 'React',
+        name: "React",
         level: 9,
       },
       {
-        name: 'Typescript',
+        name: "Typescript",
         level: 7,
       },
       {
-        name: 'GraphQL',
+        name: "GraphQL",
         level: 6,
       },
     ],
   },
   {
-    name: 'Backend development',
+    name: "Backend development",
     skills: [
       {
-        name: 'Node.js',
+        name: "Node.js",
         level: 8,
       },
       {
-        name: 'Rust',
+        name: "Rust",
         level: 5,
       },
       {
-        name: 'Golang',
+        name: "Golang",
         level: 4,
       },
     ],
   },
   {
-    name: 'Mobile development',
+    name: "Mobile development",
     skills: [
       {
-        name: 'React Native',
+        name: "React Native",
         level: 9,
       },
       {
-        name: 'Flutter',
+        name: "Flutter",
         level: 4,
       },
       {
-        name: 'Swift',
+        name: "Swift",
         level: 3,
       },
     ],
@@ -196,69 +208,69 @@ export const skills: SkillGroup[] = [
  */
 export const portfolioItems: PortfolioItem[] = [
   {
-    title: 'Project title 1',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
+    title: "Project title 1",
+    description: "Give a short description of your project here.",
+    url: "https://reactresume.com",
     image: porfolioImage1,
   },
   {
-    title: 'Project title 2',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
+    title: "Project title 2",
+    description: "Give a short description of your project here.",
+    url: "https://reactresume.com",
     image: porfolioImage2,
   },
   {
-    title: 'Project title 3',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
+    title: "Project title 3",
+    description: "Give a short description of your project here.",
+    url: "https://reactresume.com",
     image: porfolioImage3,
   },
   {
-    title: 'Project title 4',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
+    title: "Project title 4",
+    description: "Give a short description of your project here.",
+    url: "https://reactresume.com",
     image: porfolioImage4,
   },
   {
-    title: 'Project title 5',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
+    title: "Project title 5",
+    description: "Give a short description of your project here.",
+    url: "https://reactresume.com",
     image: porfolioImage5,
   },
   {
-    title: 'Project title 6',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
+    title: "Project title 6",
+    description: "Give a short description of your project here.",
+    url: "https://reactresume.com",
     image: porfolioImage6,
   },
   {
-    title: 'Project title 7',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
+    title: "Project title 7",
+    description: "Give a short description of your project here.",
+    url: "https://reactresume.com",
     image: porfolioImage7,
   },
   {
-    title: 'Project title 8',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
+    title: "Project title 8",
+    description: "Give a short description of your project here.",
+    url: "https://reactresume.com",
     image: porfolioImage8,
   },
   {
-    title: 'Project title 9',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
+    title: "Project title 9",
+    description: "Give a short description of your project here.",
+    url: "https://reactresume.com",
     image: porfolioImage9,
   },
   {
-    title: 'Project title 10',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
+    title: "Project title 10",
+    description: "Give a short description of your project here.",
+    url: "https://reactresume.com",
     image: porfolioImage10,
   },
   {
-    title: 'Project title 11',
-    description: 'Give a short description of your project here.',
-    url: 'https://reactresume.com',
+    title: "Project title 11",
+    description: "Give a short description of your project here.",
+    url: "https://reactresume.com",
     image: porfolioImage11,
   },
 ];
@@ -268,39 +280,51 @@ export const portfolioItems: PortfolioItem[] = [
  */
 export const education: TimelineItem[] = [
   {
-    date: 'April 2007',
-    location: 'Clown college',
-    title: 'Masters in Beer tasting',
-    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
+    date: "April 2007",
+    location: "Clown college",
+    title: "Masters in Beer tasting",
+    content: (
+      <p>
+        Describe your experience at school, what you learned, what useful skills
+        you have acquired etc.
+      </p>
+    ),
   },
   {
-    date: 'March 2003',
-    location: 'School of Business',
-    title: 'What did you study 101',
-    content: <p>Describe your experience at school, what you learned, what useful skills you have acquired etc.</p>,
+    date: "March 2003",
+    location: "School of Business",
+    title: "What did you study 101",
+    content: (
+      <p>
+        Describe your experience at school, what you learned, what useful skills
+        you have acquired etc.
+      </p>
+    ),
   },
 ];
 
 export const experience: TimelineItem[] = [
   {
-    date: 'March 2010 - Present',
-    location: 'Awesome Development Company',
-    title: 'Senior UX Engineer',
+    date: "March 2010 - Present",
+    location: "Awesome Development Company",
+    title: "Senior UX Engineer",
     content: (
       <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
+        Describe work, special projects, notable achievements, what technologies
+        you have been working with, and anything else that would be useful for
+        an employer to know.
       </p>
     ),
   },
   {
-    date: 'March 2007 - February 2010',
-    location: 'Garage Startup Studio',
-    title: 'Junior bug fixer',
+    date: "March 2007 - February 2010",
+    location: "Garage Startup Studio",
+    title: "Junior bug fixer",
     content: (
       <p>
-        Describe work, special projects, notable achievements, what technologies you have been working with, and
-        anything else that would be useful for an employer to know.
+        Describe work, special projects, notable achievements, what technologies
+        you have been working with, and anything else that would be useful for
+        an employer to know.
       </p>
     ),
   },
@@ -313,19 +337,22 @@ export const testimonial: TestimonialSection = {
   imageSrc: testimonialImage,
   testimonials: [
     {
-      name: 'John Doe',
-      text: 'Use this as an opportunity to promote what it is like to work with you. High value testimonials include ones from current or past co-workers, managers, or from happy clients.',
-      image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/169.jpg',
+      name: "John Doe",
+      text: "Use this as an opportunity to promote what it is like to work with you. High value testimonials include ones from current or past co-workers, managers, or from happy clients.",
+      image:
+        "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/169.jpg",
     },
     {
-      name: 'Jane Doe',
-      text: 'Here you should write some nice things that someone has said about you. Encourage them to be specific and include important details (notes about a project you were on together, impressive quality produced, etc).',
-      image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/14.jpg',
+      name: "Jane Doe",
+      text: "Here you should write some nice things that someone has said about you. Encourage them to be specific and include important details (notes about a project you were on together, impressive quality produced, etc).",
+      image:
+        "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/14.jpg",
     },
     {
-      name: 'Someone else',
-      text: 'Add several of these, and keep them as fresh as possible, but be sure to focus on quality testimonials with strong highlights of your skills/work ethic.',
-      image: 'https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/69.jpg',
+      name: "Someone else",
+      text: "Add several of these, and keep them as fresh as possible, but be sure to focus on quality testimonials with strong highlights of your skills/work ethic.",
+      image:
+        "https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/69.jpg",
     },
   ],
 };
@@ -335,28 +362,29 @@ export const testimonial: TestimonialSection = {
  */
 
 export const contact: ContactSection = {
-  headerText: 'Get in touch.',
-  description: 'Here is a good spot for a message to your readers to let them know how best to reach out to you.',
+  headerText: "Get in touch.",
+  description:
+    "Here is a good spot for a message to your readers to let them know how best to reach out to you.",
   items: [
     {
       type: ContactType.Email,
-      text: 'reachout@timbaker.me',
-      href: 'mailto:reachout@timbaker.me',
+      text: "reachout@timbaker.me",
+      href: "mailto:reachout@timbaker.me",
     },
     {
       type: ContactType.Location,
-      text: 'Victoria BC, Canada',
-      href: 'https://www.google.ca/maps/place/Victoria,+BC/@48.4262362,-123.376775,14z',
+      text: "Victoria BC, Canada",
+      href: "https://www.google.ca/maps/place/Victoria,+BC/@48.4262362,-123.376775,14z",
     },
     {
       type: ContactType.Instagram,
-      text: '@tbakerx',
-      href: 'https://www.instagram.com/tbakerx/',
+      text: "@tbakerx",
+      href: "https://www.instagram.com/tbakerx/",
     },
     {
       type: ContactType.Github,
-      text: 'tbakerx',
-      href: 'https://github.com/tbakerx',
+      text: "tbakerx",
+      href: "https://github.com/tbakerx",
     },
   ],
 };
@@ -365,9 +393,25 @@ export const contact: ContactSection = {
  * Social items
  */
 export const socialLinks: Social[] = [
-  {label: 'Github', Icon: GithubIcon, href: 'https://github.com/tbakerx'},
-  {label: 'Stack Overflow', Icon: StackOverflowIcon, href: 'https://stackoverflow.com/users/8553186/tim-baker'},
-  {label: 'LinkedIn', Icon: LinkedInIcon, href: 'https://www.linkedin.com/in/timbakerx/'},
-  {label: 'Instagram', Icon: InstagramIcon, href: 'https://www.instagram.com/reactresume/'},
-  {label: 'Twitter', Icon: TwitterIcon, href: 'https://twitter.com/TimBakerx'},
+  { label: "Github", Icon: GithubIcon, href: "https://github.com/tbakerx" },
+  {
+    label: "Stack Overflow",
+    Icon: StackOverflowIcon,
+    href: "https://stackoverflow.com/users/8553186/tim-baker",
+  },
+  {
+    label: "LinkedIn",
+    Icon: LinkedInIcon,
+    href: "https://www.linkedin.com/in/timbakerx/",
+  },
+  {
+    label: "Instagram",
+    Icon: InstagramIcon,
+    href: "https://www.instagram.com/reactresume/",
+  },
+  {
+    label: "Twitter",
+    Icon: TwitterIcon,
+    href: "https://twitter.com/TimBakerx",
+  },
 ];
